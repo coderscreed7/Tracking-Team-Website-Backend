c7c7a88836ef254f21641f0efd4201c14b26a72b
not-AdityaRawat <mynameisrawat0@gmail.com>
2025-12-12 06:18:10 +0530

    Fix CORS for Vercel frontend 2

--- api.js
+++ api.js
@@ -11,12 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: [
-        'https://tracking-team.vercel.app/',
-        'https://trackingteam.netlify.app',
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:4173'
+    origin: ['*'
     ],
     credentials: true
 }));
